d7afbe45aacaeb20f4b2799906e2e445fd8ebb44
Janithya Dias <it21203176@my.sliit.lk>
2025-08-21 09:37:59 +0530

    Folder structure and the setup

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+// Load env variables first
+dotenv.config();
+
+const connectDB = require("./config/db");
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/vehicles", require("./routes/vehicleRoutes"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
